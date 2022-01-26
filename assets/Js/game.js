ff572e7aab91e88844c70e2e70e2ae9eebc80024
Tiffany Bryan <tiffanym.bryan@gmail.com>
2022-01-25 21:19:28 -0500

    done

---
+++ assets/Js/game.js
@@ -0,0 +1,47 @@
+var questions = [
+    { 
+        question: "How do you write 'Hello World' in an alert box?", 
+        answers: [
+            { text: "msg('Hello World')", correct: false },
+            { text: "alert('Hello World')" , correct: true },
+            { text: "prompt('Hello World')", correct: false },
+            { text: "alertBox('Hello World')", correct: false }
+        ]
+    },
+    { 
+        question: "What does the symbols var stand for?", 
+        answers: [
+            { text: "lets", correct: false },
+            { text: "function", correct: false },
+            { text: "variable", correct: true },
+            { text: "javascript", correct: false }
+        ]
+    },
+    { 
+        question: "How to write an IF statement for executing some code if 'i' is NOT equal to 8?", 
+        answers: [
+            { text: "if (i != 8)", correct: true },
+            { text: "if i =! 8", correct: false },
+            { text: "if (i <> 8)", correct: false },
+            { text: "if (i !=== 8)", correct: false }
+        ]
+    },
+    { 
+        question: "What is the correct way to write a JavaScript array?", 
+        answers: [
+            { text: "var vegetable = (0:'kale', 1:'lettuce', 2:'spinach')", correct: false },
+            { text: "var vegetable = ['kale', 'lettuce', 'spinach']", correct: true },
+            { text: "var vegetable = (kale, lettuce, spinach)", correct: false },
+            { text: "None of the above", correct: false }
+        ]
+    },
+    { 
+        question: "How do we run a Javascript function by adding what to the function?",
+        answers: [
+            { text: " A .", correct: false },
+            { text: " A {}", correct: false },
+            { text: "A #", correct: false },
+            { text: "A () ", correct: true }
+        ]
+    },
+];
